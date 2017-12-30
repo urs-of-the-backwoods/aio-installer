416a07596ed688da506ce376ae0bac247e428915
Urs of the Backwoods <uotbw@hgamer3d.org>
2017-12-30 20:01:50 +0100

    updated windows script with self-elevation

--- windows/install.bat
+++ windows/install.bat
@@ -1,1 +1,8 @@
-copy aio.exe C:\Windows\System32\aio.exe
+@ECHO OFF
+:checkPrivileges 
+NET FILE 1>NUL 2>NUL
+if '%errorlevel%' == '0' ( goto gotPrivileges 
+) else ( powershell "saps -filepath %0 -verb runas" >nul 2>&1)
+exit /b 
+:gotPrivileges
+copy %~dp0\aio.exe C:\Windows\System32\aio.exe
