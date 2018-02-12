310a841b8b57011b630c24c6d22267ba5ead8a5c
Peter Althainz <urs-of-the-backwoods@users.noreply.github.com>
2018-02-12 10:57:53 +0100

    export path in bash_profile

--- darwin/install.sh
+++ darwin/install.sh
@@ -1,3 +1,6 @@
-cp $(dirname $0)/aio /usr/local/bin
+#!/bin/bash
+mkdir -p ~/bin
+cp $(dirname $0)/aio ~/bin
+echo 'export PATH=/usr/local/bin:$PATH' >>~/.bash_profile
+source ~/.bash_profile
 aio version
-
